d1190f7c146b8f7317b7d950e0bb0adc190dc771
AhmedKhaled6900 <you@example.com>
2024-08-31 17:03:13 +0300

    new

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -16,8 +16,7 @@ import { Form,
 import { RegisterSchema } from "@/schemas"
 import { Input } from "../ui/input"
 import { Button } from "../ui/button"
-// import { FormError } from "../form-error"
-// import { FormSuccess } from "../form-success"
+
 import { register } from "@/actions/register"
 import { useState, useTransition } from "react"
 import {CardWrapper} from "./card-wraper"
